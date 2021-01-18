ec6e861b55ff6d431a915c258da006b967d81de6
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-01-18 16:55:53 +0000

    Created erb file and upated rb file

--- app.rb
+++ app.rb
@@ -1,16 +1,14 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 
-get '/' do
-    "Hello World"
-  end
+  # get '/' do
+  #   "Hello World"
+  # end
 
-  get '/secret' do
-  "Salar and Oscar are cool"
-  end
+  # get '/secret' do
+  # "Salar and Oscar are cool"
+  # end
   
   get '/cat' do
-    "<div style='border: 3px solid green'>
-      <img src='https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg'>
-     </div>"
+    erb(:index)
   end
