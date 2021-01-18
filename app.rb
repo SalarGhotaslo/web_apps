909e27114e1443f233f986464fa106aad63afcb9
Salar Ghotaslo <Salar_@live.co.uk>
2021-01-18 16:35:20 +0000

    Returning HTML completed

--- app.rb
+++ app.rb
@@ -9,3 +9,8 @@ get '/' do
   "Salar and Oscar are cool"
   end
   
+  get '/cat' do
+    "<div style='border: 3px solid green'>
+      <img src='https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg'>
+     </div>"
+  end
